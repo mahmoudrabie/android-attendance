6e1eb93403107a455376e3ee1b0af29ab78a099e
adelina.vidovici@gmail.com <adelina.vidovici@gmail.com@f166cfc4-b05a-1137-6f94-3fc3a7bbba58>
2012-06-20 07:49:53 +0000

    Attend lab/course

---
+++ trunk/Attendance/Good/AttendanceNCIT/src/attendance/project/AttendanceNCITActivity4.java
@@ -0,0 +1,22 @@
+package attendance.project;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+
+public class AttendanceNCITActivity4 extends Activity{	
+	    
+	    @Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.page4);
+	        
+	    }
+
+} 
