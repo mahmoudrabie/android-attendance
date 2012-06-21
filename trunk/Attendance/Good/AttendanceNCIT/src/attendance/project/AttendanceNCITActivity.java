c848a8e9f8e484e649c886b27f09f758b48e0dde
prejbeanu.razvan@gmail.com <prejbeanu.razvan@gmail.com@f166cfc4-b05a-1137-6f94-3fc3a7bbba58>
2012-06-21 10:01:19 +0000

    Renamed classes for explicity

--- trunk/Attendance/Good/AttendanceNCIT/src/attendance/project/AttendanceNCITActivity.java
+++ trunk/Attendance/Good/AttendanceNCIT/src/attendance/project/AttendanceNCITActivity.java
@@ -30,7 +30,7 @@ public class AttendanceNCITActivity extends Activity {
 				public void onClick(View arg0) {
 					//Starting a new Intent
 					//Intent nextScreen = new Intent(getApplicationContext(), AttendanceNCITActivity2.class);
-					Intent nextScreen = new Intent(AttendanceNCITActivity.this,AttendanceNCITActivity2.class);
+					Intent nextScreen = new Intent(AttendanceNCITActivity.this,MainMenuActivity.class);
 					//Sending data to another Activity
 					//nextScreen.putExtra("username_input", inputUsername.getText().toString());
 					//nextScreen.putExtra("password_input", inputPassword.getText().toString());
